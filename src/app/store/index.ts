9be94e858cea6dcba15b8d4c495801b9c98bb503
WaleedNadeem07 <waleednadeem07@gmail.com>
2025-08-24 00:17:35 +0500

    Added State Management

---
+++ src/app/store/index.ts
@@ -0,0 +1,9 @@
+// store/index.ts
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './authSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
